32c3a61fe37e20005e4cf45f51a0a903c9456e21
Ayeeessshhhaaaaa <ayeshafaz90@gmail.com>
2024-06-15 19:15:02 +0530

    updates

--- src/Components/Question/question8.jsx
+++ src/Components/Question/question8.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const question8 = () => {
   return (
-    <div>question8</div>
+    <div className='question'>
+    <div className="question-title">
+      <span>8<span class="material-symbols-outlined">arrow_right_alt</span></span>
+        Certifying Statement*
+    </div>
+
+    <div className="question-content">
+      <form>
+        <span>
+          I hereby acknowledge that this application form was completed by me (the individual seeking to enroll in Metana) and I did not receive help from any external sources. The responses submitted are entirely my own and based on my own reasoning. Also, I opt in to receive communication messages from Metana about my application.
+        </span>
+        <div className="form-group">
+          <input type="text" className="form-control form-input" id="email" placeholder="Type or select an option" />
+        </div>
+        <div className="button-container">
+          <button type="submit" className="btn btn-primary submit-button">OK</button>
+        </div>
+      </form>
+    </div>
+  </div>
   )
 }
 
--- src/Components/Question/question9.jsx
+++ src/Components/Question/question9.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 const Question9 = () => {
   return (
-    <div>Q</div>
+    <div className='question'>
+    <div className="question-title">
+      <span>9<span class="material-symbols-outlined">arrow_right_alt</span></span>
+      LinkedIn URL (optional)
+    </div>
+
+    <div className="question-content">
+      <form>
+        <span>
+         Here's a sniper link to make your life easy - linkedin.com (It'll open in a new tab) 🚀
+        </span>
+        <div className="form-group">
+          <input type="text" className="form-control form-input" id="email" placeholder="Type your answer here" />
+        </div>
+        <div className="button-container">
+          <button type="submit" className="btn btn-primary submit-button">OK</button>
+          <div className="support-text">
+              press <strong>Cmd ⌘ + Enter ↵</strong>
+            </div>
+        </div>
+      </form>
+    </div>
+  </div>
   )
 }
 
