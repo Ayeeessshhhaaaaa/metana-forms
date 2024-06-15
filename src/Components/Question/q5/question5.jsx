0275919e753cb541fe4e3891cc9767410057e5e4
Ayeeessshhhaaaaa <ayeshafaz90@gmail.com>
2024-06-16 01:12:21 +0530

    chnage

--- src/Components/Question/q5/question5.jsx
+++ src/Components/Question/q5/question5.jsx
@@ -33,64 +33,64 @@ const Question5 = () => {
           </span>
           <div className="form-group-row">
             <div className="custom-radio">
-              <input type="checkbox" id="salary-1" value="$30,000" onChange={handleCheckboxChange} />
-              <label htmlFor="salary-1" className="custom-control-label"><span>A</span>Solidity</label>
+              <input type="checkbox" id="lang-1" value="Solidity" onChange={handleCheckboxChange} />
+              <label htmlFor="lang-1" className="custom-control-label"><span>A</span>Solidity</label>
             </div>
             <div className="custom-radio">
-              <input type="checkbox" id="salary-2" value="$30,000-$50,000" onChange={handleCheckboxChange} />
-              <label htmlFor="salary-2" className="custom-control-label"><span>B</span>Rust</label>
+              <input type="checkbox" id="lang-2" value="Rust" onChange={handleCheckboxChange} />
+              <label htmlFor="lang-2" className="custom-control-label"><span>B</span>Rust</label>
             </div>
             <div className="custom-radio">
-              <input type="checkbox" id="salary-3" value="$50,000-$80,000" onChange={handleCheckboxChange} />
-              <label htmlFor="salary-3" className="custom-control-label"><span>C</span>Node.js</label>
+              <input type="checkbox" id="lang-3" value="Node.js" onChange={handleCheckboxChange} />
+              <label htmlFor="lang-3" className="custom-control-label"><span>C</span>Node.js</label>
             </div>
             <div className="custom-radio">
-              <input type="checkbox" id="salary-4" value="$80,000-$120,000" onChange={handleCheckboxChange} />
-              <label htmlFor="salary-4" className="custom-control-label"><span>D</span>Typescript</label>
+              <input type="checkbox" id="lang-4" value="Typescript" onChange={handleCheckboxChange} />
+              <label htmlFor="lang-4" className="custom-control-label"><span>D</span>Typescript</label>
             </div>
             <div className="custom-radio">
-              <input type="checkbox" id="salary-5" value="$120,000-$250,000" onChange={handleCheckboxChange} />
-              <label htmlFor="salary-5" className="custom-control-label"><span>E</span>Javascript</label>
+              <input type="checkbox" id="lang-5" value="Javascript" onChange={handleCheckboxChange} />
+              <label htmlFor="lang-5" className="custom-control-label"><span>E</span>Javascript</label>
             </div>
             <div className="custom-radio">
-              <input type="checkbox" id="salary-6" value="$250,000+" onChange={handleCheckboxChange} />
-              <label htmlFor="salary-6" className="custom-control-label"><span>F</span>C</label>
+              <input type="checkbox" id="lang-6" value="C" onChange={handleCheckboxChange} />
+              <label htmlFor="lang-6" className="custom-control-label"><span>F</span>C</label>
             </div>
             <div className="custom-radio">
-              <input type="checkbox" id="salary-6" value="$250,000+" onChange={handleCheckboxChange} />
-              <label htmlFor="salary-6" className="custom-control-label"><span>G</span>C++</label>
+              <input type="checkbox" id="lang-7" value="C++" onChange={handleCheckboxChange} />
+              <label htmlFor="lang-7" className="custom-control-label"><span>G</span>C++</label>
             </div>
             <div className="custom-radio">
-              <input type="checkbox" id="salary-6" value="$250,000+" onChange={handleCheckboxChange} />
-              <label htmlFor="salary-6" className="custom-control-label"><span>H</span>C#</label>
+              <input type="checkbox" id="lang-8" value="C#" onChange={handleCheckboxChange} />
+              <label htmlFor="lang-8" className="custom-control-label"><span>H</span>C#</label>
             </div>
             <div className="custom-radio">
-              <input type="checkbox" id="salary-6" value="$250,000+" onChange={handleCheckboxChange} />
-              <label htmlFor="salary-6" className="custom-control-label"><span>I</span>SQL</label>
+              <input type="checkbox" id="lang-9" value="$250,000+" onChange={handleCheckboxChange} />
+              <label htmlFor="lang-9" className="custom-control-label"><span>I</span>SQL</label>
             </div>
             <div className="custom-radio">
-              <input type="checkbox" id="salary-6" value="$250,000+" onChange={handleCheckboxChange} />
-              <label htmlFor="salary-6" className="custom-control-label"><span>J</span>Python</label>
+              <input type="checkbox" id="lang-10" value="SQL" onChange={handleCheckboxChange} />
+              <label htmlFor="lang-10" className="custom-control-label"><span>J</span>Python</label>
             </div>
             <div className="custom-radio">
-              <input type="checkbox" id="salary-6" value="$250,000+" onChange={handleCheckboxChange} />
-              <label htmlFor="salary-6" className="custom-control-label"><span>K</span>Assembly Language</label>
+              <input type="checkbox" id="lang-11" value="Assembly Language" onChange={handleCheckboxChange} />
+              <label htmlFor="lang-11" className="custom-control-label"><span>K</span>Assembly Language</label>
             </div>
             <div className="custom-radio">
-              <input type="checkbox" id="salary-6" value="$250,000+" onChange={handleCheckboxChange} />
-              <label htmlFor="salary-6" className="custom-control-label"><span>L</span>Haskell</label>
+              <input type="checkbox" id="lang-12" value="Haskell" onChange={handleCheckboxChange} />
+              <label htmlFor="lang-12" className="custom-control-label"><span>L</span>Haskell</label>
             </div>
             <div className="custom-radio">
-              <input type="checkbox" id="salary-6" value="$250,000+" onChange={handleCheckboxChange} />
-              <label htmlFor="salary-6" className="custom-control-label"><span>M</span>R</label>
+              <input type="checkbox" id="lang-13" value="R" onChange={handleCheckboxChange} />
+              <label htmlFor="lang-13" className="custom-control-label"><span>M</span>R</label>
             </div>
             <div className="custom-radio">
-              <input type="checkbox" id="salary-6" value="$250,000+" onChange={handleCheckboxChange} />
-              <label htmlFor="salary-6" className="custom-control-label"><span>N</span>.NET</label>
+              <input type="checkbox" id="lang-14" value=".NET" onChange={handleCheckboxChange} />
+              <label htmlFor="lang-14" className="custom-control-label"><span>N</span>.NET</label>
             </div>
             <div className="custom-radio">
-              <input type="checkbox" id="salary-6" value="$250,000+" onChange={handleCheckboxChange} />
-              <label htmlFor="salary-6" className="custom-control-label"><span>O</span>Other</label>
+              <input type="checkbox" id="lang-15" value="Other" onChange={handleCheckboxChange} />
+              <label htmlFor="lang-15" className="custom-control-label"><span>O</span>Other</label>
             </div>
           </div>
           <div className="button-container">
--- src/Components/Question/q6/question6.jsx
+++ src/Components/Question/q6/question6.jsx
@@ -1,8 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './question6.scss';
+
 
 const Question6 = () => {
+  const [selectedOptions, setSelectedOptions] = useState([]);
+
+  const handleCheckboxChange = (e) => {
+    const value = e.target.value;
+    setSelectedOptions((prevSelectedOptions) =>
+      prevSelectedOptions.includes(value)
+        ? prevSelectedOptions.filter((option) => option !== value)
+        : [...prevSelectedOptions, value]
+    );
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission, e.g., save the selected options
+    console.log('Selected options:', selectedOptions);
+  };
+
   return (
-    <div>question6</div>
+    <div className='question6'>
+    <div className="question-title">
+      <span>6<span class="material-symbols-outlined">arrow_right_alt</span></span>
+      How would you describe your current level of coding experience?
+    </div>
+
+    <div className="question-content">
+      <form>
+        <div className="form-group">
+            <div className="custom-radio">
+                  <input type="checkbox" id="exp-1" value="No experience" onChange={handleCheckboxChange} />
+                  <label htmlFor="exp-1" className="custom-control-label"><span>A</span>No experience (I have never programmed before.)</label>
+            </div>
+            <div className="custom-radio">
+              <input type="checkbox" id="exp-2" value="Beginner" onChange={handleCheckboxChange} />
+              <label htmlFor="exp-2" className="custom-control-label"><span>B</span>Beginner (I have played with some introductory coding lessons and tutorials.)</label>
+            </div>
+            <div className="custom-radio">
+              <input type="checkbox" id="exp-3" value="Intermediate" onChange={handleCheckboxChange} />
+              <label htmlFor="exp-3" className="custom-control-label"><span>C</span>Intermediate (I have completed some coding classes or tutorials.)</label>
+            </div>
+            <div className="custom-radio">
+              <input type="checkbox" id="exp-4" value="Advanced" onChange={handleCheckboxChange} />
+              <label htmlFor="exp-4" className="custom-control-label"><span>D</span>Advanced (I can build applications.)</label>
+            </div>
+            <div className="custom-radio">
+              <input type="checkbox" id="exp-5" value="Professional" onChange={handleCheckboxChange} />
+              <label htmlFor="exp-5" className="custom-control-label"><span>E</span>Professional (I am an experienced software engineer.)</label>
+            </div>
+        </div>
+        <div className="button-container">
+          <button type="submit" className="btn btn-primary submit-button">OK</button>
+        </div>
+      </form>
+    </div>
+  </div>
   )
 }
 
