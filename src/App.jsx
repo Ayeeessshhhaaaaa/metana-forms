ce82b7f4ef4828d309e5baa884aec2c3e3e7b242
Ayeeessshhhaaaaa <ayeshafaz90@gmail.com>
2024-06-15 13:34:15 +0530

    hnn

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/home';
 import Header from './Components/Header/header';
+import Loader from './Components/Loader/loader';
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000); // simulate loading time
+  }, []);
+
   return (
     <Router>
       <div className="App">
+      {loading ? <Loader /> : <div>
         <Header />
         <div className="content">
           <Routes>
             <Route path="/" element={<Home />} />
           </Routes>
         </div>
+        </div>}
       </div>
     </Router>
   );
---
+++ src/Components/Loader/loader.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import './loader.scss';
+
+const Loader = () => {
+
+  return (
+      <div className='overlay'>
+      <div className="loader">
+      <img src='https://images.typeform.com/images/LztDQu9MkiiN' alt="Buildform Logo" className="loader-image" />
+      <div class="loader-icon"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
+
+
